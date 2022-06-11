9a9d5e6d5545e7b9c47340f85f51368a185c96ce
AdeFathiAmirrasyid <adefathi.amirrasyid11@gmail.com>
2022-06-11 17:57:11 +0700

    belajar_dart_application

---
+++ Dart/belajar_dart_library/example/hello_02.dart
@@ -0,0 +1,11 @@
+import 'package:belajar_dart_library/hello_02.dart';
+
+void main(){
+  print(sayHello("insani"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+
+}
---
+++ Dart/belajar_dart_library/lib/hello_02.dart
@@ -0,0 +1,4 @@
+library belajar_dart_library;
+
+export 'src/say_hello_01.dart';
+export 'src/customer_02.dart' show Category, Product, Customer;
